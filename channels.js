155423a8d32ae8d38989e7bafb4564e614c5dde8
JanaYW <ga95muh@mytum.de>
2017-04-07 15:40:40 +0200

    Add files via upload

---
+++ channels.js
@@ -0,0 +1,49 @@
+
+var channels = [yummy,sevencontinents,killerapp,firstpersononmars,octoberfest];
+
+var yummy = {
+    name: "#Yummy",
+    createdOn: new Date(2016, 04, 01),  
+    createdBy: "minus.plus.yummy",
+    starred: false,
+    expiresIn: 100,
+    messageCount: 999
+}
+
+/** #7 #chlob #fve: the other channels */
+
+var sevencontinents = {
+    name: "#SevenContinents",
+    createdOn: new Date(2016, 05, 01),  
+    createdBy: "cheeses.yard.applies",
+    starred: true,
+    expiresIn: 80,
+    messageCount: 640
+}
+
+var killerapp = {
+    name: "#KillerApp",
+    createdOn: new Date(2016, 06, 01), 
+    createdBy: "lodge.bits.fake",
+    starred: false,
+    expiresIn: 21,
+    messageCount: 322
+}
+
+var firstpersononmars = {
+    name: "#FirstPersonOnMars",
+    createdOn: new Date(2016, 07, 01), 
+    createdBy: "snipped.atom.grid",
+    starred: true,
+    expiresIn: 41,
+    messageCount: 289
+}
+
+var octoberfest = {
+    name: "#Octoberfest",
+    createdOn: new Date(2016, 08, 01),
+    createdBy: "vocally.clearly.crawled",
+    starred: false,
+    expiresIn: 45,
+    messageCount: 18
+}
